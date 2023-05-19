981ad9df710cd86611edc423ef666c95647cb570
Ali Koleiny Zadeh <a.koleinyz@gmail.com>
2023-05-19 15:43:02 +0330

    1. Two Sum

---
+++ src/1_Two_Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] TwoSum(int[] nums, int target)
+{
+    int[] output = new int[2];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                output[0] = i;
+                output[1] = j;
+                return output;
+            }
+        }
+    }
+    return output;
+}
+
+foreach(int a in TwoSum([3, 2, 4], 6))
+{
+    Console.WriteLine(a);
+}
