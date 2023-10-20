50391a4a4426a5b3a72a74f528e801d75f9222ef
Ali Koleiny Zadeh <a.koleinyz@gmail.com>
2023-10-20 15:43:02 +0330

    35. Search Insert Position

---
+++ src/35_Search_Insert_Position/Program.cs
@@ -0,0 +1,13 @@
+﻿int SearchInsert(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (target <= nums[i])
+        {
+            return i;
+        }
+    }
+    return nums.Length;
+}
+
+Console.WriteLine(SearchInsert([1, 3, 5, 6], 5));
