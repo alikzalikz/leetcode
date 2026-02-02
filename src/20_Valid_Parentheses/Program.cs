5649a26137285f35d4baecfb4eec91b1439ff74c
Ali Koleiny Zadeh <a.koleinyz@gmail.com>
2026-02-02 13:20:39 +0330

    20. Valid Parentheses

---
+++ src/20_Valid_Parentheses/Program.cs
@@ -0,0 +1,31 @@
+﻿bool IsValid(string s)
+{
+    if (s.Length % 2 != 0) return false;
+    Stack<char> chars = new Stack<char>();
+
+    foreach (char c in s)
+    {
+        if (c == '(' || c == '{' || c == '[')
+        {
+            chars.Push(c);
+        }
+        else
+        {
+            if (chars.Count() == 0) return false;
+            var top = chars.Peek();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '['))
+            {
+                return false;
+            }
+
+            chars.Pop();
+        }
+    }
+
+    return chars.Count() == 0;
+}
+
+string s = "[()()]{}";
+Console.WriteLine((IsValid(s)));
